fbe1106c58883545ba4abc0cea7e57f785bb0c27
jefrialbukhori <bangjeff200505@gmail.com>
2025-03-14 15:27:03 +0700

    Add files via upload

---
+++ Lab2 AP/luasppanjang.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float luas, p, l;
+
+    cout << "masukkan panjang : ";
+    cin >> p;
+
+    cout << "masukkan lebar : ";
+    cin >> l;
+
+    luas = p * l;
+    cout << "luas = " << luas << endl;
+
+    return 0;
+}
---
+++ Lab2 AP/operator.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a, b;
+    system ("CLS");
+    a = 3;
+    b = 5;
+    //int tambah = a + b;
+    //int kurang = a - b;
+    // int kali = a * b;
+    // float bagi =(float) a /(float) b;
+    // int modulo = a % b;
+
+    // cout << "hasil penjumlahan = " << tambah << endl;
+    // cout << "hasil pengungaran = " << kurang << endl;
+    // cout << "hasil perkalian = " << kali << endl;
+    // cout << "hasil pembagian = " << bagi << endl;
+    // cout << "hasil sisa bagi = " << modulo << endl;
+
+    //relational oprator
+    //cout << (a==b) << endl;
+    //cout << (a<b) << endl;
+    //cout << (a<=b) << endl;
+    //cout << (a>b) << endl;
+    //cout << (a>=b) << endl;
+    //cout << (a=b) << endl;
+
+    //logical oprator
+    //cout << (true && true) << endl;
+    //cout << (true && false) << endl;
+    //cout << (false && true) << endl;
+    //cout << (false && false) << endl;
+    
+    //cout << !true << endl;
+    //cout << !false << endl;
+
+    // bitwise operator (&, |, ^, ~, >>, <<) --> biner
+    //cout << (5%7) << endl;
+    //cout << (5|7) << endl;
+    //cout << (5^7) << endl;
+    //cout << (~7) << endl;
+    //cout << (7 << 2) << endl;
+    //cout << (7 >> 2) << endl;
+    //cout << (9 << 2) << endl;
+    //cout << (6 >> 3) << endl;
+
+    //shorthand
+    // a += 7; //a = a + 7;
+    // cout << a << endl;
+
+    // a -= 7; //a = a - 7;
+    // cout << a << endl;
+
+    // a *= 7; //a = a * 7;
+    // cout << a << endl;
+
+    // a /= 7; //a = a / 7;
+    // cout << a << endl;
+
+    //increment , decrement
+
+    //post increment
+    // cout << a << endl;
+    // cout << a++ << endl;
+
+    // cout << b << endl;
+    // cout << b++ << endl;
+
+    //post decrement
+    cout << a << endl;
+    cout << a-- << endl;
+
+    cout << b << endl;
+    cout << b-- << endl;
+}
